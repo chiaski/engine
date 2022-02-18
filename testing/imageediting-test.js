e0915b8221058548ab3b9eb60d6ffa6dc3133c9e
Chia Amisola <chiamisola@gmail.com>
2022-02-18 16:08:40 -0500

    fix drag, putting object controls into function

--- testing/imageediting-test.js
+++ testing/imageediting-test.js
@@ -9,30 +9,75 @@
 
 $( ".obj" ).dblclick(function() {
   
-  $(this)
-    .css("box-shadow", "0 0 20px #fff")
-    .data("selected", "1")
-    .draggable({
-    grid: [40,40],
-    containment: "#e",
-    stop: function(){
-      
-      updatePos(this);
-    }
-    });
+  // only one object may be selected at a time
   
-  
-  function updatePos(t){
+  if( $(this).data("selected") == "1" ){
     
-    let pos = $(t).position();
-    console.log(pos);
+    objControls.clearSelected();
     
-    $(".__x").text(pos.left);
-    $(".__y").text(pos.top);
+  } else{
+    
+    $(".controls-selected").fadeIn("slow");
+    
+    $(this)
+      .css("box-shadow", "0 0 20px #fff")
+      .data("selected", "1")
+      .draggable({
+      grid: [40,40],
+      containment: "#e",
+      stop: function(){
+        objControls.updatePos(this);
+      }
+      });
+
+
   }
-  
+
   
 });
 
+
+// object controls
+const objControls = {
+  // t = object
+  
+  // remove object
+  delete: function(t){
+    t.remove();
+  },
+  
+  // OTHER HANDLERS
+  
+  updatePos: function(t){
+      let pos = $(t).position();
+      console.log(pos);
+
+      $(".__x").text(pos.left);
+      $(".__y").text(pos.top);
+  },
+  
+  // resets all objects on dom
+  clearSelected: function(){
+    
+    
+    $(".controls-selected").fadeOut("slow");
+    
+    // reset position
+    
+      $(".__x").text("");
+      $(".__y").text("");
+    
+    $("#e .obj").each(function(){
+    
+      $(this)
+      .css("box-shadow", "none")
+      .data("selected", "0")
+      .draggable('disable');
+      
+    });
+    
+  }
+  
+}
 
 // handle image selection
