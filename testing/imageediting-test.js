07a3d2f56a05a2b256be1673f299c9f275d8b225
Chia Amisola <chiamisola@gmail.com>
2022-02-18 16:37:10 -0500

    add really shitty grow/shrink

--- testing/imageediting-test.js
+++ testing/imageediting-test.js
@@ -11,7 +11,9 @@ $( ".obj" ).dblclick(function() {
   
   // only one object may be selected at a time
   
-  if( $(this).data("selected") == "1" ){
+  
+  // deselect image
+  if( $(this).attr("data-selected") == "1" ){
     
     objControls.clearSelected();
     
@@ -21,7 +23,7 @@ $( ".obj" ).dblclick(function() {
     
     $(this)
       .css("box-shadow", "0 0 20px #fff")
-      .data("selected", "1")
+      .attr("data-selected", "1")
       .draggable({
       disabled: false,
       grid: [40,40],
@@ -42,9 +44,46 @@ $( ".obj" ).dblclick(function() {
 const objControls = {
   // t = object
   
-  // remove object
-  delete: function(t){
-    t.remove();
+  // simple handlers
+  
+  
+  // remove object 
+  del: function(t){
+    
+    if(t){
+      t.remove();
+    } else{
+    console.log("Object removed");
+      $(".obj[data-selected='1']").remove();
+    }
+    
+    // add error handling here, catch if no objec twas removed
+    
+    $(".controls-selected").fadeOut("slow");
+    
+  },
+  
+  size: function(how){
+    
+    let t = $("img.obj[data-selected='1']");
+    
+    if(how == "up"){
+      
+      console.log("grow");
+      
+      console.log(t.css("width"));
+      
+      t.css("width", t.width() * 1.25 + "px" );
+      t.css("height", t.height() * 1.25 + "px" );
+      
+    } else{
+      
+      console.log("shrink");
+      
+      t.css("width", t.width() * 0.75 + "px" );
+      t.css("height", t.height() * 0.75 + "px" );
+    }
+    
   },
   
   // OTHER HANDLERS
@@ -79,6 +118,10 @@ const objControls = {
     
   }
   
-}
+};
 
 // handle image selection
+
+
+// click handlers here
+$("button[data-action='delete']").on('click', objControls.del() );
