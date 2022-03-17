d614876a651e58ce350532200456a1ec801252c4
Chia Amisola <chiamisola@gmail.com>
2022-03-17 09:39:04 -0400

    fAWkkK

--- testing/library.js
+++ testing/library.js
@@ -1,2 +1,21 @@
 
 console.log("library.js loaded");
+
+
+/*
+
+
+  LIBRARY
+  
+
+*/
+
+
+// pathing: environment / ...
+
+const library = {
+  
+  environment: ["stone-1", "flower-1", "stone-2", "moon-1"]
+  
+  
+};
