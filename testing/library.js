1c1ccc9229db31b556f50398022cd896fce44d29
Chia Amisola <chiamisola@gmail.com>
2022-03-17 10:06:29 -0400

    add selector

--- testing/library.js
+++ testing/library.js
@@ -15,7 +15,52 @@ console.log("library.js loaded");
 
 const library = {
   
-  environment: ["stone-1", "flower-1", "stone-2", "moon-1"]
+  environment: ["stone-1", "flower-1", "stone-2", "moon-1"],
   
+  clouds: ["1"]
   
 };
+
+const libraryControls = {
+  
+  $LIBRARY_OBJECTS: "library-objects",
+  
+  init: function(){
+    
+    console.log(library);
+
+    
+  },
+  
+  load: function(what){
+    
+        
+    library[what].forEach(function(e, i){
+      
+      console.log(e);
+      
+      $("#" + libraryControls.$LIBRARY_OBJECTS).append("<img class='_toadd' src='assets/image/" + what + "/" + e + ".gif'>" );
+      
+      
+      
+    });
+    
+  }
+  
+  
+}
+
+/*
+
+
+
+    INITIALIZE AND REFERENCE HERE
+    
+    
+
+*/
+
+libraryControls.init();
+libraryControls.load('environment');
+
+
