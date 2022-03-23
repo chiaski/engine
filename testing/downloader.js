243bb37acf94b8c79172a18fba104bbb7f5cf41a
Chia Amisola <chiamisola@gmail.com>
2022-03-23 16:54:47 -0400

    Update downloader.js

--- testing/downloader.js
+++ testing/downloader.js
@@ -1,5 +1,24 @@
 
 
+$("#btn-download").on("click", function(){
+  
+  console.log("Downloading game to cartridge...");
+  
+  
+  
+  $("#downloader").text(JSON.stringify( scenes ));
+  
+  /* Select the text field */
+  $("#downloader").select();
+//  $("#downloader").setSelectionRange(0, 99999); // for mobile
+  
+   document.execCommand("copy");
+
+  alert("Downloading HTML file of game!");
+  
+});
+
+
 $("#btn-getcode").on("click", function(){
   
   console.log("Downloading game to cartridge...");
