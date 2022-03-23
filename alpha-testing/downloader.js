3952a7e02d40bf2095335772d2c350f8fcc898d9
Chia Amisola <chiamisola@gmail.com>
2022-03-23 18:33:40 -0400

    adjust downloader

--- alpha-testing/downloader.js
+++ alpha-testing/downloader.js
@@ -12,7 +12,6 @@ $("#btn-download").on("click", function(){
   // replace content with cartridge code
   $iframe.contents().find('html #cartridge').html(cartridge_code);
   
-//  
   let link = document.createElement('a');
   link.setAttribute('download', "myCartridge.html");
   link.setAttribute('href', 'data:text/plain' + ';charset=utf-8,' + encodeURIComponent( $iframe.contents().find("html").html() ));
@@ -24,15 +23,17 @@ $("#btn-download").on("click", function(){
 $("#btn-getcode").on("click", function(){
   
   console.log("Downloading game to cartridge...");
+  var cartridge_code = JSON.stringify( scenes );
   
-  $("#downloader").text(JSON.stringify( scenes ));
+  $("#downloader").text(cartridge_code);
   
-  /* Select the text field */
-  $("#downloader").select();
-//  $("#downloader").setSelectionRange(0, 99999); // for mobile
-  
-   document.execCommand("copy");
+    var range = document.createRange();
+    range.selectNode(document.getElementById("downloader"));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
 
-  alert("Copied cartridge code to clipboard!");
+   document.execCommand("copy");
+    window.getSelection().removeAllRanges();
   
+  alert("Copied cartridge code to clipboard! Note that if you have funky characters or if my code was just inadequate, this might have failed... :(");
 });
