5677726f90aca9f4f3f73acccae18140d04a44b4
Chia Amisola <chiamisola@gmail.com>
2022-04-09 17:51:01 -0400

    Tutorial designm

---
+++ alpha-testing/tutorial.js
@@ -0,0 +1,18 @@
+console.log("tutorials.js loaded");
+
+
+/*
+
+  TUTORIAL TYPES
+  s = suggestion
+  t = tip
+
+*/
+
+$('#outer-modals   ._modal').on('click', function () {
+
+  $(this)
+    .css("transform", "scale(0.98)")
+    .fadeOut(500);
+
+});
