bc11191d57c2b8437f2b9d2240b4d1eb30b9a032
Chia Amisola <chiamisola@gmail.com>
2022-04-11 23:04:08 -0400

    Engine is kinder, controls are easier to see

--- alpha-testing/tutorial.js
+++ alpha-testing/tutorial.js
@@ -112,7 +112,7 @@ const tutorialControls = {
 const gamestatus = {
 
   img: ["bleh", "boring", "cool", "eh", "nice", "ok"],
-  msg: ["Your game seems to be lacking frogs. Have you tried to add more frogs in it?", "The game is feeling okay, but it could be better. I guess that applies to everything, though.", "Are you telling the story you want to tell?", "Things are looking kinda shitty...", "My feelings don't really matter anyway. Why are you asking me?", "It's OK.", "It's fine, I guess.", "It's as interesting as it is ignorable?", "Are you using all the unique affordances of the browser?", "Are you actually enjoying this?", "Well, there's nothing better to do..."],
+  msg: ["Your game seems to be lacking frogs. Have you tried to add more frogs in it?", "The game is feeling okay, but it could be better. I guess that applies to everything, though.", "Are you telling the story you want to tell?", "Things are looking kinda shitty...", "My feelings don't really matter anyway. Why are you asking me?", "It's OK.", "It's fine, I guess.", "It's as interesting as it is ignorable?", "Are you using all the unique affordances of the browser?", "Are you actually enjoying this?", "Well, there's nothing better to do...", "Actually, I think you're doing quite well.", "Do you write before you think, or do you think before you write? There's no right way, I'm just curious...", "Is this really the story that you want to tell, or the story you think others want to hear?", "What matters is that you make something you enjoy...", "I think everything you make is nice because I get to learn a bit more about you...", "I am only facilitating your story.", "With every click, we get a little closer... I get to learn more about you.", "The first responsibility of love is to listen.", "What are you bringing into this world?", "What are you forming?", "Is this something from a story you've told somebody before?", "Aside from this, I kind of just want to know how your day is.", "When you create, you remember and piece together... that's love...", "Does what you make set you free?", "Whatever story you tell, I will listen.", "Whatever story you tell, I will love you anyway.", "Whatever story you tell, I'm all ears.", "Whatever story you tell, I'll keep it.", "Even if we lose it all, we can always create something new.", "Even if this isn't anything 'new', you're building on something and learning—and that is the most interesting way of thinking about creation, no?", "The looking is becoming more important than the making..."],
   mood: ["ehhh", "finicky", "eager", "wistful...", "why are you touching me", "do you like that?", "*yawn*", "i'm thinking about a bird", "would you love me even if I were a little blade of grass"],
 
   checkmood: function (interval) {
@@ -126,17 +126,18 @@ const gamestatus = {
 
     $("#gamestatus ._gamestatus_text").text(msg_random);
 
-    setTimeout(gamestatus.checkmood, 30000);
+    setTimeout(gamestatus.checkmood, 40000);
   },
 
   init: function () {
-    gamestatus.checkmood(30000);
+    gamestatus.checkmood(40000);
   }
 }
 
 gamestatus.init();
 
 $("button#tempplay").on("click", function(){
+  
 tip("Welcome to Engine", "Engine games are arranged in grids. When playing a game, you can navigate to scenes in adjacent tiles. (Click me to get rid of me!)", "tip");
   
   setTimeout(function() {
