9d9a70060c9727a00b61a0c204e1b53d4bbf1e6b
Chia Amisola <chiamisola@gmail.com>
2022-04-12 05:43:36 -0400

    more fonts

--- alpha-testing/library.js
+++ alpha-testing/library.js
@@ -26,7 +26,7 @@ const libraryText = {
   $SAVEbtn: "#btn-savetextoverlay",
   $TEXT_EDITOR: "e textarea",
   $TEXT: "e #e-text",
-  fonts: ["Times Now", "Comic Sans MS", "Arial", "FA Sysfont C", "Helvetica", "Courier New"],
+  fonts: ["Times Now", "Comic Sans MS", "Arial", "FA Sysfont C", "Helvetica", "Courier New", "Georgia", "Garamond", "Tahoma", "Arial Black"],
 
   init: function () {
     // click, edit text
